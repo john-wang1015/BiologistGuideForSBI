ee790f481adb2d541290eda0a018b3ab1b6b2e84
RyanJafefKelly <ryan@kiiii.com>
2023-11-09 14:36:14 +1000

    Correct typing for MATLAB simulator

--- BVCBM_HPC/simulator.m
+++ BVCBM_HPC/simulator.m
@@ -1,11 +1,11 @@
 function x = simulator(param,length_of_simulation)
 
-starting_vol = 100
-page = 2
+starting_vol = double(100);
+page = int32(2);
 
 x = zeros(1,length_of_simulation);
 
-x_temp = clib.Model.FullSimulation_biphasic(param(1), param(2), param(3), param(4), page, param(6), param(7), param(8), param(9), param(5), starting_vol, length_of_simulation);
+x_temp = clib.Model.FullSimulation_biphasic(param(1), param(2), int32(param(3)), int32(param(4)), int32(page), param(6), param(7), int32(param(8)), int32(param(9)), param(5), starting_vol, int32(length_of_simulation));
 
 for i = 1:length_of_simulation
     x(i) = x_temp(i);
