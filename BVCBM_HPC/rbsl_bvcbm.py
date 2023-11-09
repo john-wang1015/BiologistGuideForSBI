38ebcd39fdcd5621f3b01c179bcb4ee2f2b27555
Ryan Kelly <ryan@kiiii.com>
2023-11-09 13:09:33 +1000

    Update BSL - pre-sampling, log_summary, visualisations

--- BVCBM_HPC/rbsl_bvcbm.py
+++ BVCBM_HPC/rbsl_bvcbm.py
@@ -5,9 +5,10 @@ import os
 from elfi.methods.bsl import pre_sample_methods, pdf_methods
 from scipy.io import loadmat
 import multiprocessing
+from functools import partial
+import matplotlib.pyplot as plt
 
-
-def sim_fn(g_age, g_age_2, tau, matlab_engine=None, batch_size=1, random_state=None):
+def bvcbm_simulation(g_age, g_age_2, tau, eng=None, batch_size=1, random_state=None):
     t1 = 1.0
     t2 = 0.0
     t3 = 17.3
@@ -22,8 +23,9 @@ def sim_fn(g_age, g_age_2, tau, matlab_engine=None, batch_size=1, random_state=N
     theta = np.array([t1, t2, t3, g_age, tau, t6, t7, t8, g_age_2])
 
     length_of_simulation = 32
-
-    eng = matlab.engine.start_matlab()
+    start_matlab = False if eng is not None else True
+    if start_matlab:
+        eng = matlab.engine.start_matlab()
     bvcbm_path = os.path.abspath('BVCBM_HPC')
     model_path = os.path.join(bvcbm_path, 'Model')
     eng.addpath(bvcbm_path, nargout=0)
@@ -37,7 +39,8 @@ def sim_fn(g_age, g_age_2, tau, matlab_engine=None, batch_size=1, random_state=N
         sx = np.array(sx).flatten()
         sx_all[batch_ii, :] = sx
 
-    eng.quit()
+    if start_matlab:
+        eng.quit()
 
     return sx_all
 
@@ -52,6 +55,8 @@ def get_model(true_params=None, x_obs=None):
     if true_params is None:
         true_params = np.array([300.0, 100.0,  16.0])
 
+    eng = matlab.engine.start_matlab()
+    sim_fn = partial(bvcbm_simulation, eng=eng)
     y = sim_fn(*true_params)
 
     m = elfi.ElfiModel()
@@ -63,21 +68,27 @@ def get_model(true_params=None, x_obs=None):
     elfi.Simulator(sim_fn, m['g_age'], m['g_age2'],  m['tau'], observed=y, name='BVCBM')
     # summary
     elfi.Summary(sum_fn, m['BVCBM'], name='S')
+    elfi.Summary(np.log(sum_fn), m['BVCBM'], name='log_S')
     # distance
     elfi.Distance('euclidean', m['S'], name='d')
     return m
 
 
 def run_rbsl():
-    # TODO! Need to parallelise to run on HPC
     m = get_model()
     likelihood = pdf_methods.robust_likelihood("variance")
-    nsim_round = 500
-    feature_names = ['BVCBM']
+    feature_names = ['log_S']
     seed = 1
-    # elfi.set_client('multiprocessing')
+    elfi.set_client('multiprocessing')
     # print("cores: ", multiprocessing.cpu_count())
-    true_params = np.array([300.0, 100.0,  16.0])
+    # true_params = np.array([300.0, 100.0,  16.0])
+
+    n_sim = 10000
+    true_params = {'g_age': 300.0, 'g_age2': 100.0, 'tau': 16.0}
+    pre_sample_methods.plot_features(m, true_params, n_sim, feature_names,
+                                     seed=seed)
+    plt.savefig('rbslv_bvcbm_features.png')
+    plt.clf()
     n_sim = [100, 300, 500, 1000]
     log_stdev = pre_sample_methods.log_SL_stdev(model=m,
                              theta=true_params,
@@ -87,24 +98,49 @@ def run_rbsl():
                              M=20,
                              seed=123)
     print('log_stdev: ', log_stdev)
-    robust_bsl = elfi.BSL(m,
-                          nsim_round,
-                          feature_names=feature_names,
-                          likelihood=likelihood,
-                          seed=seed)
+
+    nsim_round = 500
+    r_bsl_v = elfi.BSL(m,
+                       nsim_round,
+                       batch_size=50,
+                       feature_names=feature_names,
+                       likelihood=likelihood,
+                       seed=seed
+                       )
     mcmc_iterations = 100
     est_posterior_cov = loadmat('cov_matrix_subset.mat')
     logit_transform_bound = [(2, (32*24)-1),
                              (2, 31),
                              (2, (32*24)-3)]
-    res = robust_bsl.sample(mcmc_iterations,
-                            est_posterior_cov,
-                            burn_in=10,
-                            # param_names=['g_age', 'tau', 'g_age2'],
-                            params0=true_params,
-                            logit_transform_bound=logit_transform_bound,
-                            )
+    res = r_bsl_v.sample(mcmc_iterations,
+                         est_posterior_cov,
+                         burn_in=10,
+                         param_names=['g_age', 'g_age2', 'tau'],
+                         params0=true_params,
+                         logit_transform_bound=logit_transform_bound,
+                         bar=False
+                         )
     print(res)
+
+    print(res.compute_ess())
+
+    res.plot_traces()
+    plt.savefig('rbslv_bvcbm_traces.png')
+
+
+    mbins = 30
+    res.plot_marginals(reference_value=true_params,
+                       bins=mbins)
+    plt.savefig('rbslv_bvcbm_marginals.png')
+
+    res.plot_pairs(reference_value=true_params,
+                   bins=mbins)
+    plt.savefig('rbslv_bvcbm_pairs.png')
+
+    gamma_dict = dict(zip(['gamma_{}'.format(index) for index in range(r_bsl_v.observed.size)],
+                          np.transpose(res.outputs['gamma'])))
+    elfi.visualization.visualization.plot_marginals(gamma_dict, bins=mbins)
+    plt.savefig('rbslv_bvcbm_gamma.png')
 
 
 if __name__ == '__main__':
